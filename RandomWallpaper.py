dbe8e64879eb0e7764cc24f8a83707cdb639428a
Galaxy-z <63529170+Galaxy-z@users.noreply.github.com>
2020-04-12 23:00:51 +0800

    Update RandomWallpaper.py

--- RandomWallpaper.py
+++ RandomWallpaper.py
@@ -1,14 +1,16 @@
 import requests
 import os.path
 import ctypes
+import json
 
 # 获得壁纸
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36 Edg/80.0.361.69'
 }
-url = 'https://bing.ioliu.cn/v1/rand'
+url = 'https://bing.ioliu.cn/v1/rand?callback=json'
 page = requests.get(url, headers=headers)
-img = page.content
+imgurl = json.loads(page.text.split(' json')[2][1:-2])['data']['url']
+img = requests.get(imgurl, headers=headers).content
 # 保存图片
 imgname = 'bingwallpaper.jpg'
 with open(imgname, 'wb') as f:
